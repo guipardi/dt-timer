84cba2cb05ae39de17e7c3be4232f8f6b48ff2b0
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-04 14:20:58 -0300

    Subindo restante do setup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/styles"
+import { defaultTheme } from "./styles/themes/DefaultTheme"
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
