0ad9c3360f83ae309871a0ae9098501a94e67b4c
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-05 09:15:33 -0300

    Subindo formatações de preços e datas

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -22,9 +23,14 @@ export function Transactions() {
               return (
                 <tr key={transaction.id}>
                   <td width='50%'>{transaction.description}</td>
-                  <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '-'}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
---
+++ src/utils/formatter.ts
@@ -0,0 +1,9 @@
+export const dateFormatter = new Intl.DateTimeFormat('pt-BR')
+
+export const priceFormatter = new Intl.NumberFormat(
+  'pt-Br',
+  {
+    style: 'currency',
+    currency: 'BRL'
+  }
+)
