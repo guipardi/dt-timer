3f30d33ab245f405d9a62972a4a6354113629b08
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-04 22:30:36 -0300

    Subindo contexto das transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components"
+import { TransactionsProvider } from "./contexts/TransactionsContext"
 import { Transactions } from "./pages/Transactions"
 import { GlobalStyle } from "./styles/styles"
 import { defaultTheme } from "./styles/themes/DefaultTheme"
@@ -6,7 +7,9 @@ import { defaultTheme } from "./styles/themes/DefaultTheme"
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
       
       <GlobalStyle />
     </ThemeProvider>
