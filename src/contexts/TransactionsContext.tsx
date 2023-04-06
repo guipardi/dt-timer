fbaae2bd6e207dbbfb1deec10f09501d96c5c471
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-05 23:13:21 -0300

    Subindo uso do useCallback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,6 +1,23 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { api } from '../lib/axios'
 import { createContext } from 'use-context-selector'
+
+/* 
+Porque um componente renderiza?
+1) Hooks mudaram (mudou um estado, mudou um reducer, mudou um effect)
+2) Props mudam
+3) Componentes PAI rerendered
+
+Fluxo de renderização
+1) React cria o HTML da interface daquele componente
+2) Compara a ersão do HTML recriada com a versão anterior
+3) SE mudou alguma coisa, ele reescreve o HTML na tela
+
+MEMO
+Adiciona o passo 0
+0.1) Realiza um deep comparinson, basicamente verifica se as hooks ou as props mudaram,
+      se mudaram, ele vai permitir a renderização do react
+*/
 
 interface Transaction {
   id: number
@@ -33,7 +50,7 @@ export const TransactionsContext = createContext({} as TransactionsContextType)
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       params: {
         _sort: 'createdAt',
@@ -43,21 +60,24 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransactions(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  const createTransactions = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('/transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('/transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions((state) => [response.data, ...state])
-  }
+      setTransactions((state) => [response.data, ...state])
+    },
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
